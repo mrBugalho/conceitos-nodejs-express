382e6960a012524f05bd26aad02428be0f453bb1
Rafael Bugalho Lima <installshield@gmail.com>
2020-08-25 16:50:46 -0300

    Project PersonalRepos CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+//const { uuid } = require("uuidv4");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +12,86 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: "Project not found."})
+  }
+
+  const {title, url, techs} = request.body;
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  }
+
+  
+
+  //tenho certeza que likes será igual a zero e não foi passado pelo cliente.
+  //repository.likes = 0;
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex( repository=> repository.id === id);
+
+  repositories.splice(repositoryIndex, 1);
+
+  if( repositoryIndex < 0) {
+    return response.status(400).json({error: "Project not found."});
+  }
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  //Localizar o repositório pelo id recebido.
+  const repository = repositories.find( repository => repository.id === id);
+  
+
+  if(repository == undefined){
+    return response.status(400).send();
+  }
+
+  //Aumento o numero de likes.
+  repository.likes += 1; 
+
+  return response.json(repository);
 });
 
 module.exports = app;
